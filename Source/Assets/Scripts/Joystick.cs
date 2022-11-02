6ff93e987e063b225982dd1d3ef98d1947ac49be
Roman <grv1510@mail.ru>
2022-11-03 05:36:21 +1000

    created joystick controller

---
+++ Source/Assets/Scripts/Joystick.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine.EventSystems;
+
+
+namespace Core {
+
+    public class Joystick : BaseJoystick {
+        public static Joystick joystick;
+
+        protected override void Start() {
+            if (joystick == null)
+                joystick = this;
+
+            base.Start();
+            background.gameObject.SetActive(false);
+        }
+
+        public override void OnPointerDown(PointerEventData eventData) {
+            background.anchoredPosition = ScreenPointToAnchoredPosition(eventData.position);
+            background.gameObject.SetActive(true);
+            base.OnPointerDown(eventData);
+        }
+
+        public override void OnPointerUp(PointerEventData eventData) {
+            background.gameObject.SetActive(false);
+            base.OnPointerUp(eventData);
+        }
+    }
+}
